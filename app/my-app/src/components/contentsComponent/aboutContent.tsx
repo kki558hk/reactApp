344e5902bab7aa6aaf8377103ed9f27ec7e7cfd1
Kentaro Ito <kentaroito@kentaronombp.mynet>
2023-04-02 10:23:29 +0800

    aboutページのレイアウト修正

--- app/my-app/src/components/contentsComponent/aboutContent.tsx
+++ app/my-app/src/components/contentsComponent/aboutContent.tsx
@@ -7,25 +7,34 @@ import { GifBox } from '@mui/icons-material';
 import Box from '@mui/material/Box';
 import { height } from '@mui/system';
 import { AboutContentEn, AboutContentJp } from '../subcomponents/index';
+import Grid from '@mui/material/Grid';
 
 const movieId = '8OcegOGAGIs';
+
+
+
 const AboutContent = () => {
     return (
         <>
-            <Container disableGutters sx={{ minHeight: '100vh', marginTop: '4%', marginBottom: '4%' }}>
-                <Paper elevation={3} sx={{ height: '90vh' }}>
-                    <AboutContentEn />
-                    {/* <AboutContentJp /> */}
-                    <Box sx={{ marginTop: '1%', height: '50%' }} className='movieBox'>
-                        <iframe className='youtube'
-                            src={`https://www.youtube.com/embed/${movieId}?autoplay=1`}
-                            title="YouTube"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                            height='100%'
-                            width='60%'
-                        />
-                    </Box>
+            <Container disableGutters sx={{ marginTop: '3%', marginBottom: '3%' }}>
+                <Paper elevation={3} >
+                    <Grid container spacing={0}>
+                        <Grid item xs={6}>
+                            <AboutContentEn />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Box sx={{ height: '100%' }} className='movieBox'>
+                                <iframe className='youtube'
+                                    src={`https://www.youtube.com/embed/${movieId}?autoplay=1`}
+                                    title="YouTube"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                    height='60%'
+                                    width='100%'
+                                />
+                            </Box>
+                        </Grid>
+                    </Grid>
                 </Paper>
             </Container>
 
--- app/my-app/src/components/subcomponents/aboutContentEn.tsx
+++ app/my-app/src/components/subcomponents/aboutContentEn.tsx
@@ -1,74 +1,49 @@
 import Typography from '@mui/material/Typography';
+import { Container } from '@mui/system';
 
 const AboutContentEn = () => {
     return (
         <>
-            <Typography
-                variant="h3"
-                align="center"
-                color="text.primary"
-            >
-                About this website
-            </Typography>
-            <Typography
-                variant="body1"
-                align="center"
-                color="text.primary"
-                marginTop={'2%'}
-                sx={{ fontWeight: 'bold' }}
-            >
-                <Typography component="p" variant="h5">
-                    Hi,there.
+            <Container sx={{ padding: '3%' }}>
+                <Typography
+                    variant="h3"
+                    align="center"
+                    color="text.primary"
+                >
+                    About this website
                 </Typography>
-                <Typography component="p" variant="h5">
-                    This website is developed by me,a Japanese who loves
-                    to read and get inspired by famous people's quote.
+                <Typography
+                    variant="body1"
+                    align="center"
+                    color="text.primary"
+                    marginTop={'2%'}
+                    sx={{ fontWeight: 'bold', textAlign: 'justify' }}
+                >
+                    <Typography component="p" variant="h5">
+                        Hi,there.This website is developed by me,a Japanese who loves
+                        to read and get inspired by famous people's quote.
+                    </Typography>
+                    <Typography component="p" variant="h5">
+                        I like stock investing,but since the recent market crash,
+                        I kinda think I am in need of something that leads me to a right
+                        path so that I don't panic whenever people get fearful of pessimistic social
+                        sentiment.Whenever I feel get lost on investing or whatever it is, I believe this website is going to
+                        help me to avoid the wrong path.So I decided to collect famous people's quote by the way I can
+                        read them again and again whenever I need to do.
+                    </Typography>
+                    <Typography component="p" variant="h5">
+                        For investing enthusiasts,you should check out the movie below.
+                        One movie of warren buffett show us how important it is to have
+                        solid reasoning and not to forget about it.
+                    </Typography>
+                    <Typography component="p" variant="h6" sx={{ marginTop: '5%' }}>
+                        Important Notice:This website is hosted on the free cloud that seems to be still in developing and
+                        program is not tested by anyone but me.So I am going to fix it
+                        if the bugs in the program are found out.
+                    </Typography>
                 </Typography>
-            </Typography>
-            <Typography
-                variant="body1"
-                align="center"
-                color="text.primary"
-                marginTop={'1%'}
-                sx={{ fontWeight: 'bold' }}
-            >
-                Also ,I like stock investing,but since the recent market crash,
-                I kinda think I am in need of something that leads me to a right
-                path so that I don't panic whenever people get fearful of pessimistic social
-                sentiment.
-            </Typography>
-            <Typography
-                variant="body1"
-                align="center"
-                color="text.primary"
-                marginTop={'1%'}
-                sx={{ fontWeight: 'bold' }}
-            >
-                Whenever I feel get lost on investing or whatever it is, I believe this website is going to
-                help me to avoid the wrong path.So I decided to collect famous people's quote by the way I can
-                read them again and again whenever I need to do.
-            </Typography>
-            <Typography
-                variant="body1"
-                align="center"
-                color="text.primary"
-                marginTop={'1%'}
-                sx={{ fontWeight: 'bold' }}
-            >
-                For investing enthusiasts,you should check out the movie below.
-                One movie of warren buffett show us how important it is to have
-                solid reasoning and not to forget about it.
-            </Typography>
-            <Typography
-                variant="body2"
-                align="center"
-                color="text.primary"
-                marginTop={'1%'}
-            >
-                Important Notice:This website is hosted on the free cloud that seems to be still in developing and
-                program is not tested by anyone but me.So I am going to fix it
-                if the bugs in the program are found out.
-            </Typography>
+            </Container>
+
         </>
     )
 }
