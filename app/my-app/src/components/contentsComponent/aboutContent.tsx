c43d52fd0ab5b35fce01b9d9874ffdd63ca6ded9
Kentaro Ito <kentaroito@kentaronombp.mynet>
2023-04-15 01:24:03 +0800

    レイアウトを整える

--- app/my-app/src/components/contentsComponent/aboutContent.tsx
+++ app/my-app/src/components/contentsComponent/aboutContent.tsx
@@ -18,19 +18,19 @@ const AboutContent = () => {
         <>
             <Container disableGutters sx={{ marginTop: '3%', marginBottom: '3%' }}>
                 <Paper elevation={3} >
-                    <Grid container spacing={0}>
-                        <Grid item xs={6}>
+                    <Grid container spacing={1}>
+                        <Grid item xs={12}>
                             <AboutContentEn />
                         </Grid>
-                        <Grid item xs={6}>
-                            <Box sx={{ height: '100%' }} className='movieBox'>
+                        <Grid item xs={12} display={'flex'} justifyContent={'center'} alignItems={'stretch'}>
+                            <Box sx={{ paddingTop: '1%', minHeight: '300px' }} className='movieBox'>
                                 <iframe className='youtube'
                                     src={`https://www.youtube.com/embed/${movieId}?autoplay=1`}
                                     title="YouTube"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                     allowFullScreen
-                                    height='60%'
                                     width='100%'
+                                    height='100%'
                                 />
                             </Box>
                         </Grid>
