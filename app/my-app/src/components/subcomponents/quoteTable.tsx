546e48201a0bdad807cf488ce818a26197776677
Kentaro Ito <kentaroito@kentaronombp.mynet>
2023-05-13 17:52:10 +0800

    ダイアログを開いたとき、graphqlサーバにアクセスする

--- app/my-app/src/components/subcomponents/quoteTable.tsx
+++ app/my-app/src/components/subcomponents/quoteTable.tsx
@@ -7,11 +7,20 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import { gql, useQuery } from 'urql';
+import CircularProgress from '@mui/material/CircularProgress';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import { useState, useEffect } from 'react';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import IconButton from '@mui/material/IconButton';
 
 
+let quotes: quoteData[];
 
 interface Column {
-    id: 'Contents' | 'Share';
+    id: 'Quote' | 'Share';
     label: string;
     minWidth?: number;
     align?: 'right';
@@ -19,11 +28,39 @@ interface Column {
 }
 
 const columns: readonly Column[] = [
-    { id: 'Contents', label: 'Contents', minWidth: 170 },
+    { id: 'Quote', label: 'Quote', minWidth: 170 },
     { id: 'Share', label: 'Share', minWidth: 100 }
 ];
 
+const name = 'Warren Buffett';
 const QuoteTable = (props: any) => {
+
+
+    const QuoteQuery = gql`
+    query ($name :String!) {
+        QuotesByPerson(name : $name) {
+          Content
+        }
+      }
+  `;
+    const [result, reexecuteQuery] = useQuery({
+        query: QuoteQuery,
+        variables: { name },
+    });
+
+    const { data, fetching, error } = result;
+    useEffect(() => {
+        if (fetching === false && data !== undefined) {
+            quotes = data.QuotesByPerson;
+            console.log('quotes setted')
+        }
+
+    }, [data])
+
+
+
+
+
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
@@ -53,11 +90,43 @@ const QuoteTable = (props: any) => {
                                 </TableCell>
                             ))}
                         </TableRow>
-                        <TableRow>
-
-                        </TableRow>
                     </TableHead>
                     <TableBody>
+                        {
+                            fetching === true ?
+                                (
+                                    <TableRow>
+                                        <CircularProgress />
+                                    </TableRow>
+                                ) : (
+                                    error !== undefined ? (
+                                        <TableRow>
+                                            <Alert severity="error">
+                                                <AlertTitle>Error</AlertTitle>
+                                                Something went wrong!
+                                            </Alert>
+                                        </TableRow>
+                                    ) : (quotes !== undefined && fetching === false && quotes.length > 0 ?
+                                        (
+                                            quotes.map((quote: quoteData) =>
+                                                <TableRow>
+                                                    <TableCell>
+                                                        {quote.Content}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        <IconButton sx={{ marginLeft: '1%', marginRight: '5%' }}>
+                                                            <TwitterIcon />
+                                                        </IconButton>
+                                                        <IconButton>
+                                                            <InstagramIcon />
+                                                        </IconButton>
+                                                    </TableCell>
+                                                </TableRow>
+                                            )
+                                        ) : <></>
+                                    )
+                                )
+                        }
                     </TableBody>
                 </Table>
             </TableContainer>
